fae2da6a1cec772b5b0bb9a3f70871b826c027dd
doro404 <166941040+doro404@users.noreply.github.com>
2025-03-26 15:50:58 -0300

    Update abs2.js

--- abs2.js
+++ abs2.js
@@ -1,1 +1,1 @@
-(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',8957913,document.createElement('script'))
+<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',8925023,document.createElement('script'))</script>
