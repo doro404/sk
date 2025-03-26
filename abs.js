2eb9b8a6feee9f05151d87127b55feb57640f223
doro404 <166941040+doro404@users.noreply.github.com>
2025-03-26 15:41:41 -0300

    Update abs.js

--- abs.js
+++ abs.js
@@ -1,1 +1,1 @@
-    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8957911,document.createElement('script'))</script>
+<script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8957911,document.createElement('script'))</script>
